6492bea76ac4c8b8065d8cd4212665d132abc21e
Artur Barseghyan <artur.barseghyan@gmail.com>
2026-03-21 01:55:27 +0100

    More

--- src/license_normaliser/_exceptions.py
+++
@@ -1,8 +0,0 @@
-"""Public exceptions re-exported for backwards compatibility."""
-
-__author__ = "Artur Barseghyan <artur.barseghyan@gmail.com>"
-__copyright__ = "2026 Artur Barseghyan"
-__license__ = "MIT"
-__all__ = ("LicenseNormalisationError",)
-
-from .exceptions import LicenseNormalisationError
